5eda2aa55dc88625c64b180e3bb1521b057e4fea
AlexVeskov <alexveskov123@gmail.com>
2025-05-01 20:50:43 +0300

    Create UserService.java

---
+++ food_delivery/src/main/java/delivery/service/UserService.java
@@ -0,0 +1,33 @@
+package delivery.service;
+
+import delivery.User;
+import delivery.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User registerUser(User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new RuntimeException("Username already exists");
+        }
+        return userRepository.save(user);
+    }
+
+    public User login(String username, String password) {
+        User user = findUserByUsername(username);
+        if (!user.getPassword().equals(password)) {
+            throw new RuntimeException("Invalid username or password");
+        }
+        return user;
+    }
+}
