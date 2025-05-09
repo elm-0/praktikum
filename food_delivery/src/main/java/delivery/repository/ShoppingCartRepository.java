e85b26893d96894a97245d06563c0d37a6c57f4f
AlexVeskov <alexveskov123@gmail.com>
2025-05-09 12:08:03 +0300

    Update ShoppingCartRepository.java

--- food_delivery/src/main/java/delivery/repository/ShoppingCartRepository.java
+++ food_delivery/src/main/java/delivery/repository/ShoppingCartRepository.java
@@ -2,7 +2,15 @@ package delivery.repository;
 
 import delivery.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+  @Query("SELECT c FROM ShoppingCart c WHERE c.employee.id = :employeeId")
+  List<ShoppingCart> findOrderHistoryByEmployeeId(@Param("employeeId") Long employeeId);
+
+  @Query("SELECT c FROM ShoppingCart c WHERE c.employee.id = :employeeId AND c.status = 'Assigned'")
+  List<ShoppingCart> findAssignedOrdersByEmployeeId(@Param("employeeId") Long employeeId);
 }
